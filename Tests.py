d69d3d0803db592ec759e74baf700ed6b8c31ab1
Pasquale Campitiello <pasquale.campitiello@faircg.com>
2017-09-05 21:46:19 +1000

    updates

--- Tests.py
+++ Tests.py
@@ -1,25 +1,15 @@
-BOARD_SIZE = 4
+import itertools
+from typing import Iterator
 
-def under_attack(col, queens):
-    left = right = col
-    for r, c in reversed(queens):
+def iter_primes() -> Iterator[int]:
+    numbers = itertools.count(2)
+    while True:
+        prime = next(numbers)
+        yield prime
 
-        left, right = left - 1, right + 1
+        numbers = filter(prime.__rmod__, numbers)
 
-        if c in (left, col, right):
-            return True
-    return False
-
-def solve(n):
-    if n == 0:
-        return [[]]
-
-    smaller_solutions = solve(n - 1)
-
-    return [solution+[(n,i+1)]
-        for i in xrange(BOARD_SIZE)
-            for solution in smaller_solutions
-                if not under_attack(i+1, solution)]
-
-for answer in solve(BOARD_SIZE):
-    print answer
+for p in iter_primes():
+    if p > 1000:
+        break
+    print(p)
