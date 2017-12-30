7de6f90737d7164156242bd5281b710cafd500df
Pasquale Campitiello <pasquale.campitiello@faircg.com>
2017-12-30 16:35:57 +1100

    script first Google result

--- WebScraping/FirstGoogleResult.py
+++ WebScraping/FirstGoogleResult.py
@@ -1,12 +1,26 @@
-# https://stackoverflow.com/questions/33427504/how-can-i-scrape-the-first-link-of-a-google-search-with-beautiful-soup?rq=1
+# See: https://stackoverflow.com/questions/33427504/how-can-i-scrape-the-first-link-of-a-google-search-with-beautiful-soup?rq=1
 
-import requests
+import requests, urllib.parse, sys
 from bs4 import BeautifulSoup
 
-research_later = "hiya"
-goog_search = "https://www.google.co.uk/search?sclient=psy-ab&client=ubuntu&hs=k5b&channel=fs&biw=1366&bih=648&noj=1&q=" + research_later
+#research_later = "Zygmunt Bauman"
+research_later = sys.argv[1]
+google_search = "https://www.google.com.au/search?q=" + urllib.parse.quote_plus(research_later)
 
-r = requests.get(goog_search)
+read_data = requests.get(google_search)
 
-soup = BeautifulSoup(r.text, "html.parser")
-print soup.find('cite').text
+soup = BeautifulSoup(read_data.text, "html.parser")
+
+print("This URL: " + google_search + " produced:\n")
+#print(soup)
+
+# all the results are in the tags <cite>
+print("First 10 results:")
+i=1
+for cite in soup.find_all('cite'):
+    print(str(i).rjust(2) + " - " + cite.text)
+    i+=1
+
+# first result in the first tag <cite>
+print("\nFirst result: " + soup.find('cite').text)
+
