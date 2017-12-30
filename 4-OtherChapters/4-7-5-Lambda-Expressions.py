cbabf4c8b1ddb8dc4b52d4de5328c95fbd65bac1
Pasquale Campitiello <pasquale.campitiello@faircg.com>
2017-12-30 15:33:32 +1100

    readme and 4* removal

--- 4-OtherChapters/4-7-5-Lambda-Expressions.py
+++
@@ -1,8 +0,0 @@
-# It sorts the list of pairs using as key the second value of each pair.
-
-pairs = [(1, 'one'), (2, 'two'), (3, 'three'), (4, 'four')]
-pairs.sort(key=lambda pair: pair[1])
-print(pairs)
-
-# It creates a list of squares until 10
-print(list(map(lambda x: x**2, range(10))))
--- 4-OtherChapters/6-3-The-dir-function.py
+++
@@ -1,6 +0,0 @@
-import time, builtins
-print(dir(time))
-print()
-print(dir())
-print()
-print(dir(builtins))
