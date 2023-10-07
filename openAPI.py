fc95fc8a17625bb67156634c573350495f8f1d4f
pas-campitiello <pas.campitiello@gmail.com>
2023-10-07 13:49:44 +1100

    Add files via upload

---
+++ openAPI.py
@@ -0,0 +1,14 @@
+import openai
+
+# Authenticate with your API key
+openai.api_key = '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'
+
+# Make a completion request
+response = openai.Completion.create(
+  model="text-davinci-003",  # or another model of your choice
+  prompt="What is the work of a consultant with a magic stick?'",
+  max_tokens=60
+)
+
+# Print the response
+print(response.choices[0].text.strip())
